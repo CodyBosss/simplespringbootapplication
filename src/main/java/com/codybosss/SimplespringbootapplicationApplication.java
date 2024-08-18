3e28689e8cdbb8a77a6777caf133a07579b4b05a
SandyCoder <sudhir.bk9@gmail.com>
2024-08-18 22:28:23 +0530

    initial comment

---
+++ src/main/java/com/codybosss/SimplespringbootapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.codybosss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplespringbootapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplespringbootapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codybosss/SimplespringbootapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codybosss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplespringbootapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
